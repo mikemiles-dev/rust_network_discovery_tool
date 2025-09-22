3d58fd6f720a54e20553f015bb933d96f938806c
mikemiles-dev <michaelmileusnich@Michaels-MacBook-Air-3.local>
2025-09-21 23:15:25 -0500

    fix: Lookups

--- src/network/mdns_lookup.rs
+++ src/network/mdns_lookup.rs
@@ -14,73 +14,92 @@ impl MDnsLookup {
     pub fn start_daemon() {
         let mdns = ServiceDaemon::new().expect("Failed to create mDNS daemon");
 
-        let service_type = "_smb._tcp.local.";
-        let receiver = mdns
-            .browse(service_type)
-            .expect("Failed to browse for services");
+        let services_to_browse = [
+            "_afpovertcp._tcp.local.",
+            "_http._udp.local.",
+            "_http._tcp.local.",
+            "_https._udp.local.",
+            "_https._tcp.local.",
+            "_ssh._tcp.local.",
+            "_smb._tcp.local.",
+            "_homesharing._tcp.local.",
+            "_hap._tcp.local.",
+            "_ipp._tcp.local.",
+            "_airplay._tcp.local.",
+            "_googlecast._tcp.local.",
+            "_workstation._tcp.local.",
+            "_services._dns-sd._udp.local.",
+            "_immich._tcp.local.",
+        ];
 
-        let conn = new_connection();
+        for service in services_to_browse.iter() {
+            let service_type = service;
+            let receiver = mdns
+                .browse(service_type)
+                .expect("Failed to browse for services");
 
-        task::spawn_blocking(move || {
-            loop {
-                for event in receiver.iter() {
-                    match event {
-                        ServiceEvent::ServiceFound(_service, _full) => (),
-                        ServiceEvent::ServiceResolved(service_info) => {
-                            let mut host = service_info.get_hostname().to_string();
-                            if host.ends_with('.') {
-                                host.pop();
-                            }
+            let conn = new_connection();
 
-                            println!("addressed are: {:?}", service_info.get_addresses());
-                            for addr in service_info.get_addresses() {
-                                let addr = addr.to_ip_addr().to_string();
-                                if let Ok(mut lookups) = MDNS_LOOKUPS
-                                    .get_or_init(|| RwLock::new(HashMap::new()))
-                                    .write()
-                                {
-                                    lookups.insert(addr.to_string(), host.to_string());
-                                    println!("mDNS lookup added: {} -> {}", addr, host);
-                                    match EndPoint::update_hostname_by_ip(
-                                        &conn,
-                                        Some(addr.to_string()),
-                                        Some(host.to_string()),
-                                    ) {
-                                        Ok(_) => {
-                                            println!(
-                                                "Successfully updated hostname for {}: {}",
-                                                addr, host
-                                            );
-                                        }
-                                        Err(e) => {
-                                            eprintln!(
-                                                "Failed to update hostname for {}: {}",
-                                                addr, e
-                                            )
+            task::spawn_blocking(move || {
+                loop {
+                    for event in receiver.iter() {
+                        match event {
+                            ServiceEvent::ServiceFound(_service, _full) => (),
+                            ServiceEvent::ServiceResolved(service_info) => {
+                                let mut host = service_info.get_hostname().to_string();
+                                if host.ends_with('.') {
+                                    host.pop();
+                                }
+
+                                for addr in service_info.get_addresses() {
+                                    let addr = addr.to_ip_addr().to_string();
+                                    if let Ok(mut lookups) = MDNS_LOOKUPS
+                                        .get_or_init(|| RwLock::new(HashMap::new()))
+                                        .write()
+                                    {
+                                        lookups.insert(addr.to_string(), host.to_string());
+                                        println!("mDNS lookup added: {} -> {}", addr, host);
+                                        match EndPoint::update_hostname_by_ip(
+                                            &conn,
+                                            Some(addr.to_string()),
+                                            Some(host.to_string()),
+                                        ) {
+                                            Ok(_) => {
+                                                println!(
+                                                    "Successfully updated hostname for {}: {}",
+                                                    addr, host
+                                                );
+                                            }
+                                            Err(e) => {
+                                                eprintln!(
+                                                    "Failed to update hostname for {}: {}",
+                                                    addr, e
+                                                )
+                                            }
                                         }
                                     }
                                 }
+                                // Check if the service's IP addresses match our target
+                                //println!("Discovered service: {:?}", service_info);
+                                // if service_info.addresses.iter().any(|scoped_ip| {
+                                //     println!("Checking scoped IP: {}", scoped_ip);
+                                //     scoped_ip.to_string() == ip
+                                // }) {
+                                //     println!(
+                                //         "Service resolved: {}",
+                                //         service_info.get_hostname().to_string()
+                                //     );
+                                // }
                             }
-                            // Check if the service's IP addresses match our target
-                            //println!("Discovered service: {:?}", service_info);
-                            // if service_info.addresses.iter().any(|scoped_ip| {
-                            //     println!("Checking scoped IP: {}", scoped_ip);
-                            //     scoped_ip.to_string() == ip
-                            // }) {
-                            //     println!(
-                            //         "Service resolved: {}",
-                            //         service_info.get_hostname().to_string()
-                            //     );
-                            // }
+                            ServiceEvent::SearchStopped(_) => break,
+                            _ => {}
                         }
-                        ServiceEvent::SearchStopped(_) => break,
-                        _ => {}
                     }
+                    println!("Sleeping for 1 seconds before restarting mDNS browse...");
+                    std::thread::sleep(std::time::Duration::from_secs(1));
                 }
-                println!("Sleeping for 1 seconds before restarting mDNS browse...");
-                std::thread::sleep(std::time::Duration::from_secs(1));
-            }
-        });
+            });
+        }
     }
 
     pub fn lookup(ip: &str) -> Option<String> {
